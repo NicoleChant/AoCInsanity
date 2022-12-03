42d80e270dc6afd319dc5b2b11a241444b729553
NicoleChant <nicolechantzi@gmail.com>
2022-12-03 14:31:47 +0200

    Improved Solution on PartII day 3 Muhaha. Make my day

--- pyaoc/day3/solution.py
+++ pyaoc/day3/solution.py
@@ -1,32 +1,17 @@
 from pyaoc.data_source import Solution
+from functools import reduce
 
 class PartI(Solution):
 
     def solve(self):
-        return sum(map(lambda row : sum(map(lambda x :ord(x) - 96 if ord(x) > 96 else ord(x) - 38
-                , set(row[:len(row)//2]).intersection(row[len(row)//2:]))),
+        return sum(map(lambda row :
+                sum(map(lambda x :ord(x) - 96 if ord(x) > 96 else ord(x) - 38
+                ,
+                set(row[:len(row)//2]).intersection(row[len(row)//2:]))),
                 self.challenge_data.strip().split("\n")))
+
 
 class PartII(Solution):
 
-   def solve(self):
-        idx = 0
-        total = 0
-        temp = set()
-        for d in self.challenge_data.strip().split("\n"):
-            if idx%3==0:
-                temp = set(d)
-            else:
-                temp = temp.intersection(d)
-
-            if len(temp) == 1:
-                x = list(set(temp))[0]
-                total += ord(x) - 96 if ord(x) > 96 else ord(x) - 38
-            idx += 1
-        return total
-
-
-
-if __name__ == "__main__":
-    solution=PartII().solve()
-    print(solution)
+    def solve(self):
+        return sum(map(lambda group:(lambda x : ord(x) - 96 if ord(x) > 96 else ord(x) - 38)(reduce(lambda acc , val : acc.intersection(val)  , group[1:] , set(group[0])).pop()) ,zip(*[iter(self.challenge_data.strip().split("\n"))]*3)))
