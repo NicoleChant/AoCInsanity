24dc1c45f8c3d33ee399d2e0916b11082a67a098
NicoleChant <nicolechantzi@gmail.com>
2022-12-04 19:28:08 +0200

    day 4 solutions! LETS GO

--- pyaoc/day4/solution.py
+++ pyaoc/day4/solution.py
@@ -2,16 +2,16 @@ from pyaoc.data_source import Solution
 
 class PartI(Solution):
 
-    def solve(self):
-        pass
+    def solve(self) -> int:
+        return sum(map(lambda row : (lambda elf1 , elf2 : (lambda x , y , a , b : (int(a) <= int(x) and int(y) <= int(b)) or (int(x) <= int(a) and int(b) <= int(y) ) ) (*elf1.split("-"),*elf2.split("-")))(*row.split(",")) , self.challenge_data.split("\n")))
+
 
 class PartII(Solution):
 
-   def solve(self):
-        pass
-
+   def solve(self) -> int:
+        return sum(map(lambda row : (lambda elf1 , elf2 : (lambda x , y , a , b : not (int(b) < int(x) or int(y) < int(a)))(*elf1.split("-"),*elf2.split("-")))(*row.split(",")) , self.challenge_data.split("\n")))
 
 
 if __name__=="__main__":
-    solution=PartI().solve()
+    solution=PartII().solve()
     print(solution)
